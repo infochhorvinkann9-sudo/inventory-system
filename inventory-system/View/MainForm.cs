cb7803dd2fbbd15496d68b4a2452cc62c335f11a
Chhorvin Kann <info.chhorvinkann9@gmail.com>
2026-08-28 13:25:51 +0700

    update and create

--- inventory-system/View/MainForm.cs
+++ inventory-system/View/MainForm.cs
@@ -22,9 +22,58 @@ namespace inventory_system.View
 
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            uc_Container.Controls.Clear();
+            uc_Container.Dock = DockStyle.Fill;
+            uc_Dashboard dashboardControl = new uc_Dashboard();
+            uc_Container.Controls.Add(dashboardControl);
+            dashboardControl.BringToFront();
+        }
+
         private void button2_Click(object sender, EventArgs e)
         {
+            uc_Container.Controls.Clear();
+            uc_Container.Dock = DockStyle.Fill;
+            uc_Accessory accessoryControl = new uc_Accessory();
+            uc_Container.Controls.Add(accessoryControl);
+            accessoryControl.BringToFront();
+        }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            uc_Container.Controls.Clear();
+            uc_Container.Dock = DockStyle.Fill;
+            uc_Category categoryControl = new uc_Category();
+            uc_Container.Controls.Add(categoryControl);
+            categoryControl.BringToFront();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            uc_Container.Controls.Clear();
+            uc_Container.Dock = DockStyle.Fill;
+            uc_Supplier supplierControl = new uc_Supplier();
+            uc_Container.Controls.Add(supplierControl);
+            supplierControl.BringToFront();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            uc_Container.Controls.Clear();
+            uc_Container.Dock = DockStyle.Fill;
+            uc_User userControl = new uc_User();
+            uc_Container.Controls.Add(userControl);
+            userControl.BringToFront();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            uc_Container.Controls.Clear();
+            uc_Setting settingControl = new uc_Setting();
+            uc_Container.Controls.Add(settingControl);
+            settingControl.BringToFront();
         }
     }
 }
