aa9804fc7acafff952a971d9700e36f93f2251bb
Chhorvin Kann <info.chhorvinkann9@gmail.com>
2026-08-30 19:40:32 +0700

    create connection_db

---
+++ inventory-system/connection_db.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace inventory_system
+{
+    internal class connection_db
+    {
+        public SqlConnection conn;
+        public connection_db(SqlConnection conn)
+        {
+            try
+            {
+                string connectionString =
+                      @"Data Source=DESKTOP-O31OODG;
+                      Initial Catalog=inventory_db;
+                      User ID=sa;
+                      Password=12";
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection Failed!");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
