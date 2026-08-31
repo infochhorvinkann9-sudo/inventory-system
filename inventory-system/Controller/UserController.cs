d93c21be10aa2b46b7f57d77e3e1e524154171af
Chhorvin Kann <info.chhorvinkann9@gmail.com>
2026-08-31 22:13:02 +0700

    update uc user call data to display

---
+++ inventory-system/Controller/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory_system.Controller
+{
+    internal class UserController : Models.UserModel
+    {
+        // Insert Data to Table
+        public void InsertUser()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+        }
+
+        // Get and View Data User
+        public DataTable dt = new DataTable();
+        //public DataSet ds = new DataSet();
+        public SqlDataAdapter adapter = new SqlDataAdapter();
+
+        public DataTable ShowData()
+        {
+            string sql = "SELECT * FROM tblUser;";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            return dt;
+        }
+    }
+}
---
+++ inventory-system/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory_system.Models
+{
+    internal class UserModel : connection_db
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string UserRole  { get; set; }
+        public int UserStatus { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
