6981129b34ce4c3fecff795a7fdfe0a4d42bcc90
Chhorvin Kann <info.chhorvinkann9@gmail.com>
2026-08-31 22:38:16 +0700

    add cn

---
+++ inventory-system/Controller/UserController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory_system.Controller
+{
+    internal class UserController
+    {
+    }
+}
