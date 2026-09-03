56f0dde41512b43d992bfd358b33aad09bfcac60
Chhorvin Kann <info.chhorvinkann9@gmail.com>
2026-09-03 17:29:42 +0700

    fix little err

--- inventory-system/Controller/UserController.cs
+++ inventory-system/Controller/UserController.cs
@@ -13,25 +13,35 @@ namespace inventory_system.Controller
     internal class UserController : Models.UserModel
     {
         // Insert User 
-
         public void InsertUser()
         {
             try
             {
-                SqlCommand cmd = new SqlCommand();
-                cmd.Connection = conn;
-                cmd.CommandText = "InsertUser";
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add("@UserName", SqlDbType.NVarChar).Value = UserName;
-                cmd.Parameters.Add("@Password", SqlDbType.NVarChar).Value = Password;
-                cmd.Parameters.Add("@UserRole", SqlDbType.NVarChar).Value = UserRole;
-                cmd.Parameters.Add("@UserStatus", SqlDbType.Int).Value = UserStatus;
-                cmd.ExecuteNonQuery();
+                connection_db db = new connection_db();
+                if (db.conn.State != ConnectionState.Open)
+                {
+                    db.conn.Open();
+                }
+
+                using (SqlCommand cmd = new SqlCommand("InsertUser", db.conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@UserName", SqlDbType.NVarChar).Value = UserName;
+                    cmd.Parameters.Add("@Password", SqlDbType.NVarChar).Value = Password;
+                    cmd.Parameters.Add("@UserRole", SqlDbType.NVarChar).Value = UserRole;
+                    cmd.Parameters.Add("@UserStatus", SqlDbType.Int).Value = UserStatus;
+                    cmd.ExecuteNonQuery();
+                }
+
+                if (db.conn.State == ConnectionState.Open)
+                {
+                    db.conn.Close();
+                }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("User Inserted Failed: " + ex.Message, 
-                    "Cannot Insert User", MessageBoxButtons.YesNo,
+                MessageBox.Show("User Inserted Failed: " + ex.Message,
+                    "Cannot Insert User", MessageBoxButtons.OK,
                     MessageBoxIcon.Warning);
             }
         }
@@ -40,22 +50,32 @@ namespace inventory_system.Controller
         {
             try
             {
-                SqlCommand cmd = new SqlCommand();
-                cmd.Connection = conn;
-                cmd.CommandText = "UpdateUser";
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add("@UserId", SqlDbType.Int).Value = UserId;
-                cmd.Parameters.Add("@UserName", SqlDbType.NVarChar).Value = UserName;
-                cmd.Parameters.Add("@Password", SqlDbType.NVarChar).Value = Password;
-                cmd.Parameters.Add("@UserRole", SqlDbType.NVarChar).Value = UserRole;
-                cmd.Parameters.Add("@UserStatus", SqlDbType.Int).Value = UserStatus;
-                cmd.ExecuteNonQuery();
+                connection_db db = new connection_db();
+                if (db.conn.State != ConnectionState.Open)
+                {
+                    db.conn.Open();
+                }
+
+                using (SqlCommand cmd = new SqlCommand("UpdateUser", db.conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@UserId", SqlDbType.Int).Value = UserId;
+                    cmd.Parameters.Add("@UserName", SqlDbType.NVarChar).Value = UserName;
+                    cmd.Parameters.Add("@Password", SqlDbType.NVarChar).Value = Password;
+                    cmd.Parameters.Add("@UserRole", SqlDbType.NVarChar).Value = UserRole;
+                    cmd.Parameters.Add("@UserStatus", SqlDbType.Int).Value = UserStatus;
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                if (db.conn.State == ConnectionState.Open)
+                {
+                    db.conn.Close();
+                }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("User Update Failed: " + ex.Message,
-                    "Cannot Update User", MessageBoxButtons.YesNo,
-                    MessageBoxIcon.Warning);
+                throw new Exception("User Update Failed: " + ex.Message);
             }
         }
 
@@ -63,24 +83,31 @@ namespace inventory_system.Controller
         {
             try
             {
-                SqlCommand cmd = new SqlCommand();
-                cmd.Connection = conn;
-                cmd.CommandText = "DeleteUser";
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add("@UserId", SqlDbType.Int).Value = UserId;
-                cmd.ExecuteNonQuery();
+                connection_db db = new connection_db();
+                if (db.conn.State != ConnectionState.Open)
+                {
+                    db.conn.Open();
+                }
+
+                using (SqlCommand cmd = new SqlCommand("DeleteUser", db.conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@UserId", SqlDbType.Int).Value = UserId;
+                    cmd.ExecuteNonQuery();
+                }
+
+                if (db.conn.State == ConnectionState.Open)
+                {
+                    db.conn.Close();
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show("User Delete Failed: " + ex.Message,
-                    "Cannot Delete User", MessageBoxButtons.YesNo,
+                    "Cannot Delete User", MessageBoxButtons.OK,
                     MessageBoxIcon.Warning);
             }
         }
-
-
-
-
 
         public static bool IsUsernameToken(string username)
         {
@@ -96,49 +123,37 @@ namespace inventory_system.Controller
             }
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         // Get data from Users
-
         public DataTable dt = new DataTable();
         public DataSet ds = new DataSet();
         public SqlDataAdapter adapter = new SqlDataAdapter();
 
-        // Get obj
         public void GetUserData()
         {
-            string sql = "SELECT * FROM tblUsers";
-            SqlCommand cmd = new SqlCommand(sql, conn);
-            adapter.SelectCommand = cmd;
-            ds.Clear();
-            adapter.Fill(ds);
-            dt = ds.Tables[0];
+            try
+            {
+                connection_db db = new connection_db();
+                if (db.conn.State != ConnectionState.Open)
+                {
+                    db.conn.Open();
+                }
+
+                string sql = "SELECT * FROM tblUsers";
+                SqlCommand cmd = new SqlCommand(sql, db.conn);
+                adapter.SelectCommand = cmd;
+                ds.Clear();
+                adapter.Fill(ds);
+                dt = ds.Tables[0];
+
+                if (db.conn.State == ConnectionState.Open)
+                {
+                    db.conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Get User Data Failed: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
